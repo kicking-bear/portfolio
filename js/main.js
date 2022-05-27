a2149406e1e42eee3b3f564137726b216f1f1fdd
Tristan Manning <3xstan@gmail.com>
2022-05-27 12:37:32 -0400

    Made initial aesthetic changes base on the new design.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+    Daniel Manning's Portfolio Website
+    Authors: Tristan Manning, Daniel Manning
+    A showcase website for Daniel Manning's UX designs and experience.
+*/
+
+function initializeMain() {
+
+}
