5e28945539dd197d00d888b37835b0266716917e
Tristan Manning <3xstan@gmail.com>
2022-06-13 12:51:32 -0400

    Created the Recent Work card placeholders as a DOM object. Will integrate DOM manipulation into the rest of the site soon to make it more modular.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,37 @@
     A showcase website for Daniel Manning's UX designs and experience.
 */
 
+var doc = document;
+var domParser = new DOMParser();
+
+console.log(doc);
+
+const cardContent = '<div class="recent-work-card">'
+    +   '<p>This is a card</p>'
+    + '</div>';
+
+
 function initializeMain() {
 
 }
+
+function generateCardCarousel() {
+
+}
+
+function generateCard() {
+    let recentWorkCard = document.createElement('div');
+    recentWorkCard.className = 'recent-work-card';
+    //let recentWorkCard = domParser.parseFromString(cardContent, 'text/html');
+    recentWorkCard.innerHTML = "This is a DOM object";
+    return recentWorkCard;
+}
+
+function addNewCard() {
+    let card = generateCard();
+    let recentWork = document.getElementById("recent-work");
+    console.log(card);
+    recentWork.getElementsByClassName("content-block")[0].appendChild(card);
+}
+
+addNewCard();
