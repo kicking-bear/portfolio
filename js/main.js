b049904f5965438b88ac0de09cec2cbf216192c3
Tristan Manning <3xstan@gmail.com>
2022-06-25 15:25:22 -0400

    Added in the data structure for the Recent Work cards.

--- js/main.js
+++ js/main.js
@@ -7,33 +7,74 @@
 var doc = document;
 var domParser = new DOMParser();
 
-console.log(doc);
-
 const cardContent = 
     '<div class="recent-work-card">' 
-+       '<div class="rw-image"><img src="img\\filler-card-image.png" alt=""></div>'
++       '<div class="rw-image"></div>'
 +       '<div class="rw-info-block">'
-+           '<div class="rw-title">LOGO MODULE RESEARCH</div>'
-+           '<span class="rw-tag-group"><div class="rw-tag rw-red">UX Designer</div><div class="rw-tag rw-blue">UX Design</div></span>'
-+           '<div class="rw-description">Created and led the base design for one of our major front-facing business sites at Martindale-Hubbell. My design was pushed and approved by the project managers, working closely with the team to meet their business needs.</div>'
++           '<div class="rw-title"></div>'
++           '<span class="rw-tag-group"></span>'
++           '<div class="rw-description"></div>'
 +       '</div>'
 +   '</div>';
+
+const mainData =
+{
+    "colors":
+    [
+        {"UX Research": "#F8C3AC"},
+        {"UX Design": "#ACEAF8"},
+        {"UI Design": "#DFF8AC"}
+    ],
+    "projects":
+    [
+        {
+            "title": "LOGO MODULE RESEARCH",
+            "tags":
+            [
+                "UX Research",
+                "UX Design",
+                "UI Design"
+            ],
+            "imageSrc": "./img/filler-card-image.png",
+            "description":
+                "Created and led the base design for one of our major front-facing business sites at Martindale-Hubbell. My design was pushed and approved by the project managers, working closely with the team to meet their business needs."
+        },
+        {
+            "title": "LOGO MODULE RESEARCH",
+            "tags":
+            [
+                "UX Research",
+                "UX Design",
+                "UI Design"
+            ],
+            "imageSrc": "./img/filler-card-image.png",
+            "description":
+                "Created and led the base design for one of our major front-facing business sites at Martindale-Hubbell. My design was pushed and approved by the project managers, working closely with the team to meet their business needs."
+        }
+    ]
+};
+
 
 function initializeMain() {
 
 }
 
 function generateCardCarousel() {
-
+    
 }
 
 function generateCard() {
-    //let recentWorkCard = document.createElement('div');
-    //recentWorkCard.className = 'recent-work-card';
     let recentWorkCard = domParser.parseFromString(cardContent, "text/html").body.getElementsByClassName('recent-work-card')[0];
-    //recentWorkCard.innerHTML = cardTag;
-    console.log(recentWorkCard);
-    //recentWorkCard.innerHTML = "This is a DOM object";
+    let rwInfoBlock = recentWorkCard.getElementsByClassName('rw-info-block')[0];
+    let currentProject = mainData.projects[0];
+
+    recentWorkCard.getElementsByClassName('rw-image')[0].innerHTML = 
+        '<img src="' + currentProject.imageSrc + '" alt="">';
+
+        console.log(currentProject['title']);
+    
+    rwInfoBlock.getElementsByClassName('rw-title')[0].innerHTML = currentProject['title'];
+
     return recentWorkCard;
 }
 
@@ -43,5 +84,7 @@ function addNewCard() {
     //console.log(card);
     recentWork.getElementsByClassName("content-block")[0].appendChild(card);
 }
+
+initializeMain();
 
 addNewCard();
