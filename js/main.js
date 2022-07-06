67a795319e172afb3dec4f65dda00f4d76184f7d
Tristan Manning <3xstan@gmail.com>
2022-07-05 23:40:48 -0400

    Reworked the Recent Work cards to format properly and to adjust for mobile view. Made the slogan & profile picture adjust for mobile view. Added a temporary underline for the navbar menu items when the mouse hovers over them.

--- js/main.js
+++ js/main.js
@@ -4,14 +4,21 @@
     A showcase website for Daniel Manning's UX designs and experience.
 */
 
-var doc = document;
-var domParser = new DOMParser();
+var doc;
+var domParser;
+
+// Homepage Sections
+var titleBlock;
+var recentWorkBlock;
+var myStoryBlock;
+var myLifeBlock;
+
 
 console.log(doc);
 
-const cardContent = 
+const cardTemplate = 
     '<div class="recent-work-card">' 
-+       '<div class="rw-image"><img src="img\\filler-card-image.png" alt=""></div>'
++       '<div class="rw-image"><img src="img/filler-card-image.png" alt=""></div>'
 +       '<div class="rw-info-block">'
 +           '<div class="rw-title">LOGO MODULE RESEARCH</div>'
 +           '<span class="rw-tag-group"><div class="rw-tag rw-red">UX Designer</div><div class="rw-tag rw-blue">UX Design</div></span>'
@@ -20,7 +27,18 @@ const cardContent =
 +   '</div>';
 
 function initializeMain() {
+    doc = document;
+    domParser = new DOMParser();
+    generateRWSection(doc.getElementsByTagName('main')[0]);
+}
 
+function generateRWSection(mainTag) {
+    recentWorkBlock = doc.createElement('section');
+    recentWorkBlock.setAttribute('id','recent-work');
+    recentWorkBlock.classList.add('body-section', 'content-block');
+    recentWorkBlock.innerHTML = '<div class="section-title"><h2>Recent Work</h2></div>';
+
+    mainTag.appendChild(recentWorkBlock);
 }
 
 function generateCardCarousel() {
@@ -30,7 +48,7 @@ function generateCardCarousel() {
 function generateCard() {
     //let recentWorkCard = document.createElement('div');
     //recentWorkCard.className = 'recent-work-card';
-    let recentWorkCard = domParser.parseFromString(cardContent, "text/html").body.getElementsByClassName('recent-work-card')[0];
+    let recentWorkCard = domParser.parseFromString(cardTemplate, "text/html").body.getElementsByClassName('recent-work-card')[0];
     //recentWorkCard.innerHTML = cardTag;
     console.log(recentWorkCard);
     //recentWorkCard.innerHTML = "This is a DOM object";
@@ -39,9 +57,12 @@ function generateCard() {
 
 function addNewCard() {
     let card = generateCard();
-    let recentWork = document.getElementById("recent-work");
+    recentWorkBlock.appendChild(card);
+    //let recentWork = doc.getElementById("recent-work");
     //console.log(card);
-    recentWork.getElementsByClassName("content-block")[0].appendChild(card);
+    //recentWork.getElementsByClassName("content-block")[0].appendChild(card);
 }
+
+initializeMain();
 
 addNewCard();
